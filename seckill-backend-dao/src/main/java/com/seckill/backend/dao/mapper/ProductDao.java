f01fc6e3262b21f224767c28bc065b2462945689
jibojun <jbj19910123@163.com>
2018-08-18 18:03:33 +0800

    update

--- seckill-backend-dao/src/main/java/com/seckill/backend/dao/mapper/ProductDao.java
+++ seckill-backend-dao/src/main/java/com/seckill/backend/dao/mapper/ProductDao.java
@@ -1,9 +1,17 @@
 package com.seckill.backend.dao.mapper;
 
+import com.seckill.backend.dao.entity.Product;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * @Author: Bojun Ji
  * @Description:
  * @Date: 2018/8/18_5:57 PM
  */
 public interface ProductDao {
+    Product queryProductByPk(@Param("productId") long productId);
+    List<Product> queryProductByName(String productName);
+    int updateProduct(Product product);
 }
