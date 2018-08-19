17d45152ea54cfe6216f979787ae16f504c7081c
jibojun <jbj19910123@163.com>
2018-08-19 18:24:03 +0800

    update

--- seckill-backend-dao/src/main/java/com/seckill/backend/dao/sharding/DBOrderSharding.java
+++ seckill-backend-dao/src/main/java/com/seckill/backend/dao/sharding/DBOrderSharding.java
@@ -2,9 +2,11 @@ package com.seckill.backend.dao.sharding;
 
 import com.dangdang.ddframe.rdb.sharding.api.ShardingValue;
 import com.dangdang.ddframe.rdb.sharding.api.strategy.database.SingleKeyDatabaseShardingAlgorithm;
+import com.google.common.collect.Range;
 import com.seckill.backend.common.constants.CommonConstants;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
 
 /**
  * @Author: Bojun Ji
@@ -40,7 +42,15 @@ public class DBOrderSharding implements SingleKeyDatabaseShardingAlgorithm<Integ
      */
     @Override
     public Collection<String> doInSharding(Collection<String> collection, ShardingValue<Integer> shardingValue) {
-        return null;
+        Collection<String> result = new LinkedHashSet<>(collection.size());
+        for (Integer value : shardingValue.getValues()) {
+            for (String tableName : collection) {
+                if (tableName.endsWith(value % CommonConstants.DB_SHARDING_NUMBER + "")) {
+                    result.add(tableName);
+                }
+            }
+        }
+        return result;
     }
 
     /**
@@ -52,6 +62,15 @@ public class DBOrderSharding implements SingleKeyDatabaseShardingAlgorithm<Integ
      */
     @Override
     public Collection<String> doBetweenSharding(Collection<String> collection, ShardingValue<Integer> shardingValue) {
-        return null;
+        Collection<String> result = new LinkedHashSet<>(collection.size());
+        Range<Integer> range = shardingValue.getValueRange();
+        for (Integer i = range.lowerEndpoint(); i <= range.upperEndpoint(); i++) {
+            for (String each : collection) {
+                if (each.endsWith(i % CommonConstants.DB_SHARDING_NUMBER + "")) {
+                    result.add(each);
+                }
+            }
+        }
+        return result;
     }
 }
--- seckill-backend-dao/src/main/java/com/seckill/backend/dao/sharding/DBProductSharding.java
+++ seckill-backend-dao/src/main/java/com/seckill/backend/dao/sharding/DBProductSharding.java
@@ -2,9 +2,11 @@ package com.seckill.backend.dao.sharding;
 
 import com.dangdang.ddframe.rdb.sharding.api.ShardingValue;
 import com.dangdang.ddframe.rdb.sharding.api.strategy.database.SingleKeyDatabaseShardingAlgorithm;
+import com.google.common.collect.Range;
 import com.seckill.backend.common.constants.CommonConstants;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
 
 /**
  * @Author: Bojun Ji
@@ -26,11 +28,28 @@ public class DBProductSharding implements SingleKeyDatabaseShardingAlgorithm<Int
 
     @Override
     public Collection<String> doInSharding(Collection<String> availableTargetNames, ShardingValue<Integer> shardingValue) {
-        return null;
+        Collection<String> result = new LinkedHashSet<>(availableTargetNames.size());
+        for (Integer value : shardingValue.getValues()) {
+            for (String tableName : availableTargetNames) {
+                if (tableName.endsWith(value % CommonConstants.DB_SHARDING_NUMBER + "")) {
+                    result.add(tableName);
+                }
+            }
+        }
+        return result;
     }
 
     @Override
     public Collection<String> doBetweenSharding(Collection<String> availableTargetNames, ShardingValue<Integer> shardingValue) {
-        return null;
+        Collection<String> result = new LinkedHashSet<>(availableTargetNames.size());
+        Range<Integer> range = shardingValue.getValueRange();
+        for (Integer i = range.lowerEndpoint(); i <= range.upperEndpoint(); i++) {
+            for (String each : availableTargetNames) {
+                if (each.endsWith(i % CommonConstants.DB_SHARDING_NUMBER + "")) {
+                    result.add(each);
+                }
+            }
+        }
+        return result;
     }
 }
