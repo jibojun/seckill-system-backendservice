980aa4c6ba82408a348178332a55987766e60fcf
jibojun <jbj19910123@163.com>
2018-09-12 00:33:33 +0800

    update

--- seckill-backend-service/src/main/java/com/seckill/backend/service/startup/ContextLoaderListenerNew.java
+++ seckill-backend-service/src/main/java/com/seckill/backend/service/startup/ContextLoaderListenerNew.java
@@ -1,11 +1,18 @@
 package com.seckill.backend.service.startup;
 
+import com.alibaba.fastjson.JSON;
+import com.seckill.backend.common.lock.RedisPool;
+import com.seckill.backend.common.logger.LogUtil;
+import com.seckill.backend.dao.entity.Product;
 import com.seckill.backend.dao.mapper.ProductDao;
 import org.springframework.context.ApplicationContext;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import redis.clients.jedis.Jedis;
 
 import javax.servlet.ServletContextEvent;
+import java.util.List;
 
 /**
  * @Author: Bojun Ji
@@ -18,8 +25,17 @@ public class ContextLoaderListenerNew extends ContextLoaderListener {
     public void contextInitialized(ServletContextEvent event) {
         //run spring context loader listener's initialization function
         super.contextInitialized(event);
-        //TODO: load product cache from DB
+        // load product from DB, store amount cache to redis
         ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(event.getServletContext());
-        context.getBean("productDao", ProductDao.class);
+        ProductDao productDao = context.getBean("productDao", ProductDao.class);
+        List<Product> productList = productDao.queryAllProducts();
+        if (!CollectionUtils.isEmpty(productList)) {
+            Jedis jedis = RedisPool.getConnResource();
+            for (Product product : productList) {
+                //put amount to cache
+                long returnedValue = jedis.incrBy(String.valueOf(product.getProductId()), product.getAmount());
+                LogUtil.logInfo(this.getClass(), String.format("put product: [%s] to redis cache, amount is: %s", JSON.toJSONString(product), returnedValue));
+            }
+        }
     }
 }
