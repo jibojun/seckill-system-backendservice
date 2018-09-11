03a2a98d0ee9a6d206f56fd54b4d2709e802d07a
jibojun <jbj19910123@163.com>
2018-09-12 00:04:58 +0800

    update

--- seckill-backend-service/src/main/java/com/seckill/backend/service/mq/MqConsumer.java
+++ seckill-backend-service/src/main/java/com/seckill/backend/service/mq/MqConsumer.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 
 /**
  * @Author: Bojun Ji
- * @Description: kafka consumer to subscribe submit order message
+ * @Description: kafka consumer to subscribe submit order message, generate order in DB
  * @Date: 2018/8/4_2:20 AM
  */
 public class MqConsumer {
