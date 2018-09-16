c17e7e956d324e712d276937d0d25009bf54c8c5
jibojun <jbj19910123@163.com>
2018-09-17 00:57:31 +0800

    update

---
+++ seckill-backend-service/src/main/java/com/seckill/backend/service/api/IOrderIdGenService.java
@@ -0,0 +1,10 @@
+package com.seckill.backend.service.api;
+
+/**
+ * @Author: Bojun Ji
+ * @Description:
+ * @Date: 2018/9/17_12:57 AM
+ */
+public interface IOrderIdGenService {
+    long getOrderId();
+}
