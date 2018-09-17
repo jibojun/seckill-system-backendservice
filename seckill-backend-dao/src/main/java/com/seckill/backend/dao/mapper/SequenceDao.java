b4c76c7375fe245c3512d0d0978f9e98ac58c408
jibj <jibj@Ctrip.com>
2018-09-17 15:37:23 +0800

    update

--- seckill-backend-dao/src/main/java/com/seckill/backend/dao/mapper/SequenceDao.java
+++ seckill-backend-dao/src/main/java/com/seckill/backend/dao/mapper/SequenceDao.java
@@ -1,5 +1,12 @@
 package com.seckill.backend.dao.mapper;
 
-public interface SequenceDao {
+import com.seckill.backend.dao.entity.Sequence;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
+public interface SequenceDao {
+    List<Sequence> selectAllSequence();
+
+    int updateSequence(@Param("tableName") String tableName);
 }
